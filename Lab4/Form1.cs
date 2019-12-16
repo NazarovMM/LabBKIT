551100e88069b4b48397f3d3d694f1f9194931d5
NazarovMM <maxzbox1@rambler.ru>
2019-12-16 12:01:00 +0300

    Lab 4

--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Lab4
 {
@@ -15,6 +17,88 @@ namespace Lab4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        List<string> list = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                string text = File.ReadAllText(fd.FileName);
+
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (!list.Contains(str)) list.Add(str);
+                }
+                t.Stop();
+                this.textBox2.Text = t.Elapsed.ToString();
+                this.textBox3.Text = list.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string word = this.textBox4.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                this.textBox1.Text = t.Elapsed.ToString();
+                this.listBox1.BeginUpdate();
+                this.listBox1.Items.Clear();
+                foreach (string str in tempList)
+                {
+                    this.listBox1.Items.Add(str);
+                }
+                this.listBox1.EndUpdate();
+            }
+            else
+            { 
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
